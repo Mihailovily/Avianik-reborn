e4b977b3113c007f83550a20f2f04e4b8b7f945d
Ilya <mihailovily@gmail.com>
2021-06-22 04:20:13 -0700

    Migration on pug

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $("#burger").click(function() {
         $(".menu").toggle(1000);
     });
+    $("body").on('click', '[href*="#"]', function(e) {
+        var fixed_offset = 100;
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+        e.preventDefault();
+    });
 });
